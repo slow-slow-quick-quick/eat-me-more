5a381fdb47b9c8caf3db35b0513fbf901bf449c5
haloa19 <hj199514@gmail.com>
2021-06-21 21:05:11 +0900

    프로그래머스 > 소수찾기(완성)

--- HyeJ/p_12921_jin.test.js
+++ HyeJ/p_12921_jin.test.js
@@ -3,29 +3,29 @@
  * [HISTORY]
  * 테스트10~12, 효율성 시간초과
  * 에라토스테네스의 체 사용해보기
+ * 통과 (new Set() 사용해보기)
  */
 function solution(n) {
-  var answer = 0;
+  let answer = 0;
+  let nArr = [0, 1];
 
-  if (n === 2) return 1;
-
-  var nArr = [];
   for (let i = 2; i <= n; i++) {
     nArr.push(i);
   }
 
-  for (let i = 0; i < nArr.length; i++) {
-    let tmp = nArr[i];
-    for (let j = 2; j < tmp; j++) {
-      if (tmp % j === 0) {
-        nArr = nArr.filter(function (n) {
-          return n % tmp !== 0;
-        });
+  for (let i = 2; i <= n + 1; i++) {
+    if (nArr[i] != -1) {
+      for (let j = i * 2; j <= n + 1; j += i) {
+        nArr[j] = -1;
       }
     }
   }
 
-  answer = nArr.length;
+  for (let i = 0; i <= n + 1; i++) {
+    if (nArr[i] > 1) {
+      answer++;
+    }
+  }
 
   return answer;
 }
