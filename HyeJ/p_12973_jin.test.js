137f7236accd48f8bf779dee0f4d576d880999b9
haloa19 <hj199514@gmail.com>
2021-07-01 23:36:37 +0900

    프로그래머스 > 짝지어제거(완성)

--- HyeJ/p_12973_jin.test.js
+++ HyeJ/p_12973_jin.test.js
@@ -2,24 +2,26 @@
  * [프로그래머스 > 2017 팁스타운 > 짝지어 제거하기]
  * [HISTORY]
  * 실패 - 시간초과 (정규식 사용)
+ * 통과 - stack 사용
  */
 function solution(s) {
   let answer = -1;
+  let sArr = s.split("");
 
   if (s.length % 2 !== 0) return 0;
 
-  while (
-    s.match(
-      /aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz/
-    )
-  ) {
-    s = s.replace(
-      /aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz/,
-      ""
-    );
-  }
+  let stack = [];
 
-  return s === "" ? 1 : 0;
+  sArr.forEach((e) => {
+    if (stack.length > 0) {
+      if (stack[stack.length - 1] === e) stack.pop();
+      else stack.push(e);
+    } else {
+      stack.push(e);
+    }
+  });
+
+  return stack.length === 0 ? 1 : 0;
 }
 
 // 테스트
