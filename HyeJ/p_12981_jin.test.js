b865b8d8224cbfbb72de3fce2677a606dc849836
haloa19 <hj199514@gmail.com>
2021-07-04 23:31:17 +0900

    프로그래머스 > 영어끝말잇기(진행)

---
+++ HyeJ/p_12981_jin.test.js
@@ -0,0 +1,83 @@
+/*
+ * [프로그래머스 > Summer/Winter Coding(~2018) > 영어 끝말잇기]
+ * [HISTORY]
+ *  리턴값 구하는 함수 보완
+ */
+function solution(n, words) {
+  let answer = [];
+
+  let last_char = words[0].substring(words[0].length - 1);
+  let word_set = new Set();
+  word_set.add(words[0]);
+  let size = word_set.size;
+
+  function returnInfo(i) {
+    let r1 = (i + 1) % n;
+    let r2 = parseInt((i + 1) / n) + 1;
+    answer.push(r1);
+    answer.push(r2);
+    return answer;
+  }
+
+  for (let i = 1; i < words.length; i++) {
+    //console.log(words[i].substring(0, 1));
+    word_set.add(words[i]);
+    if (word_set.size === size) {
+      answer = returnInfo(i);
+      return answer;
+    } else {
+      size = word_set.size;
+    }
+    if (words[i].substring(0, 1) != last_char) {
+      answer = returnInfo(i);
+      return answer;
+    } else {
+      last_char = words[i].substring(words[i].length - 1);
+    }
+  }
+
+  return [0, 0];
+}
+
+// 테스트
+test.each([
+  [
+    3,
+    [
+      "tank",
+      "kick",
+      "know",
+      "wheel",
+      "land",
+      "dream",
+      "mother",
+      "robot",
+      "tank",
+    ],
+    [3, 3],
+  ],
+  [
+    5,
+    [
+      "hello",
+      "observe",
+      "effect",
+      "take",
+      "either",
+      "recognize",
+      "encourage",
+      "ensure",
+      "establish",
+      "hang",
+      "gather",
+      "refer",
+      "reference",
+      "estimate",
+      "executive",
+    ],
+    [0, 0],
+  ],
+  [2, ["hello", "one", "even", "never", "now", "world", "draw"], [1, 3]],
+])("연습 - 영어 끝말잇기", (n, words, result) => {
+  expect(solution(n, words)).toEqual(result);
+});
