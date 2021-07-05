040eaa1410f95ddaf229c9215336d98ec543803e
haloa19 <hj199514@gmail.com>
2021-07-05 20:23:48 +0900

    프로그래머스 > 영어끝말잇기(통과)

--- HyeJ/p_12981_jin.test.js
+++ HyeJ/p_12981_jin.test.js
@@ -1,39 +1,36 @@
 /*
  * [프로그래머스 > Summer/Winter Coding(~2018) > 영어 끝말잇기]
  * [HISTORY]
- *  리턴값 구하는 함수 보완
+ * 리턴값 구하는 함수 보완
+ * 통과
  */
 function solution(n, words) {
   let answer = [];
-
   let last_char = words[0].substring(words[0].length - 1);
   let word_set = new Set();
   word_set.add(words[0]);
   let size = word_set.size;
 
   function returnInfo(i) {
-    let r1 = (i + 1) % n;
-    let r2 = parseInt((i + 1) / n) + 1;
+    let r1 = (i + 1) % n === 0 ? n : (i + 1) % n;
+    let r2 = (i + 1) % n === 0 ? (i + 1) / n : parseInt((i + 1) / n) + 1;
     answer.push(r1);
     answer.push(r2);
     return answer;
   }
 
   for (let i = 1; i < words.length; i++) {
-    //console.log(words[i].substring(0, 1));
     word_set.add(words[i]);
     if (word_set.size === size) {
       answer = returnInfo(i);
       return answer;
-    } else {
-      size = word_set.size;
     }
-    if (words[i].substring(0, 1) != last_char) {
+    if (words[i].substring(0, 1) !== last_char) {
       answer = returnInfo(i);
       return answer;
-    } else {
-      last_char = words[i].substring(words[i].length - 1);
     }
+    size = word_set.size;
+    last_char = words[i].substring(words[i].length - 1);
   }
 
   return [0, 0];
