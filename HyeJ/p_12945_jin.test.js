c5e85bbddd03a3a3a1612a292f2f017b7b8c51c5
haloa19 <hj199514@gmail.com>
2021-12-15 22:55:50 +0900

    프로그래머스 > 피보나치 수(ing2)

--- HyeJ/p_12945_jin.test.js
+++ HyeJ/p_12945_jin.test.js
@@ -1,17 +1,25 @@
 /*
  * [프로그래머스 > 연습문제 > 피보나치 수]
  * [HISTORY]
- * 시간초과 -> n의 범위(2 <= n <= 100,000) 고려하여 재귀에서 변경
+ * 재귀함수 -> 시간초과 -> n의 범위(2 <= n <= 100,000) 고려하여 재귀에서 변경
+ * 탑다운 방식(메모제이션) -> 실패 + 런타임에러
  */
 function solution(n) {
   let answer = 0;
+  let fiboArr = new Array(100000).fill(0);
+
   answer = fibo(n);
 
   function fibo(n) {
     if (n === 1 || n === 2) {
+      fiboArr[n - 1] = 1;
       return 1;
     }
-    return fibo(n - 1) + fibo(n - 2);
+    if (fiboArr[n - 1] != 0) {
+      return fiboArr[n - 1];
+    }
+    fiboArr[n - 1] = fibo(n - 2) + fibo(n - 1);
+    return fiboArr[n - 1];
   }
 
   return answer % 1234567;
@@ -22,5 +30,5 @@ test.each([
   [3, 2],
   [5, 5],
 ])("연습문제 - 피보나치 수", (n, result) => {
-  expect(solution(n)).toEqual(result);
+  expect(solution(n)).toBe(result);
 });
